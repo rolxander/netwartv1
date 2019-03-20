c62ed44264206518e4190cd1d7a64282683d82d6
Rolaxx <gaelelpapi@gmail.com>
2019-03-20 11:14:08 -0400

    preuva 1

--- src/components/helloworld.js
+++ src/components/helloworld.js
@@ -1,7 +1,28 @@
 import React,{Component} from 'react';
 class Hello_World extends Component
 {
-render(){
-    return(<h1>Hello World</h1>);
+    constructor(props){
+        super(props);
+        console.log(props)
+        this.state = {
+            count : 0
+        };
+        this.handleCountCLick = this.handleCountCLick.bind(this);
+    }
+    componentDidMount(){
+        this.setState({count : 1});
+    }
+    handleCountCLick(){        
+        this.setState({count : this.state.count+1});
+    }
+render(){    
+    const {mensaje} = this.props.mensaje;
+    const title = this.props.title;
+    console.log(this.state.count+ "estamos entrando al render");
+    return(<div>
+                <h1>{mensaje} {title} </h1>
+                <h2> {this.state.count}</h2>
+                <button onClick={this.handleCountCLick}>click</button>
+            </div>);
 }
 }export default Hello_World;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './css/estilos.scss';
 import React from 'react';
 import {render} from 'react-dom';
 import Hello_World from '../src/components/helloworld.js';
-render(<Hello_World/>, document.getElementById("root"));
+const mensaje = {mensaje : "hola mundo"};
+render(<Hello_World mensaje={mensaje} title="jaja"/>, document.getElementById("root"));
